4766b7294b99916295cfe17e8cc57172c32c47c2
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2025-08-03 15:12:21 +0545

    feat: shows commands in the center instead of the bottom

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,8 @@
+-- This is a plugin that will show commands as popup instead of in the cmdline
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    }
+}
