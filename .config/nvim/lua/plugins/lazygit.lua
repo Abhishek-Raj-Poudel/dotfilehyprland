22ec388122bc66d6cb2bcaff1d1168846a1e6515
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2025-10-16 10:46:23 +0545

    feat: some feature with kitty

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -1,20 +1,28 @@
 return {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+	"kdheepak/lazygit.nvim",
+	lazy = true,
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	-- optional for floating window border decoration
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+
+	config = function()
+		-- This will make lazygit use your terminal colors
+		vim.g.lazygit_floating_window_winblend = 0
+		vim.g.lazygit_floating_window_scaling_factor = 0.9
+		vim.g.lazygit_floating_window_border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+		vim.g.lazygit_use_neovim_remote = 1
+	end,
+	-- setting the keybinding for LazyGit with 'keys' is recommended in
+	-- order to load the plugin when the command is run for the first time
+	keys = {
+		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+	},
 }
