6112846ded0fc7d0f9e2958cf09775d108f0a7bc
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2025-10-20 12:40:58 +0545

    feat: add word rename feature

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -11,12 +11,16 @@ return {
 			{
 				"neovim/nvim-lspconfig",
 
-				-- config = function()
-				--     vim.keymap.set('n', '<leader>f', function()
-				--         vim.lsp.buf.format({ async = true })
-				--     end
-				--     , { desc = "Find Files in your project" })
-				-- end
+				config = function(event)
+					local map = function(keys, func, desc, mode)
+						mode = mode or "n"
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+
+					-- Rename the variable under your cursor.
+					--  Most Language Servers support renaming across files, etc.
+					map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+				end,
 			},
 		},
 	},
