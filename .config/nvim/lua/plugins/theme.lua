66d38032cde318f72e696bffda1c61ff1adad950
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2025-11-11 16:24:05 +0545

    some shit

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -6,15 +6,15 @@ return {
 			vim.cmd("colorscheme kanagawa-dragon")
 			vim.cmd("colorscheme kanagawa-dragon")
 			vim.cmd([[
-  highlight LineNr guibg=NONE
-  highlight CursorLineNr guibg=NONE
-  highlight CursorLine guibg=NONE
-  highlight GitSignsAddNr guibg=NONE
-  highlight GitSignsChangeNr guibg=NONE
-  highlight GitSignsDeleteNr guibg=NONE
-  highlight GitSignsChangedeleteNr guibg=NONE
-  highlight GitSignsTopdeleteNr guibg=NONE
-]])
+            highlight LineNr guibg=NONE
+            highlight CursorLineNr guibg=NONE
+            highlight CursorLine guibg=NONE
+            highlight GitSignsAddNr guibg=NONE
+            highlight GitSignsChangeNr guibg=NONE
+            highlight GitSignsDeleteNr guibg=NONE
+            highlight GitSignsChangedeleteNr guibg=NONE
+            highlight GitSignsTopdeleteNr guibg=NONE
+            ]])
 			-- vim.cmd("colorscheme kanagawa-lotus")
 		end,
 	},
